3fde9058750afc5023b1a6b16ee3cb9b979a666f
Klint Kanopka <klint.kanopka@gmail.com>
2021-02-08 16:24:48 -0800

    new bias plot

--- code/bias_plots.R
+++ code/bias_plots.R
@@ -1,16 +1,25 @@
 library(tidyverse)
 
-results <- readRDS('code/sim_data/sim_allele_cont.rds')
+setwd('~/projects/phen-diff')
 
 results %>%
-  filter(N==500) %>%
   select(N, b, case, b_pd, b_fe, b_ols) %>%
   pivot_longer(starts_with('b_'), names_to='model', values_to='estimate') %>%
-  ggplot(aes(x=b, y=estimate)) +
-  geom_point(color='grey', alpha=0.05) +
-  geom_smooth(aes(color=model), method=lm, formula=y~x) +
-  geom_abline(aes(slope=1, intercept=0), lty=2, color='black') +
-  facet_grid(model~case) +
-  theme_bw()
+  mutate(case = factor(case, levels=c('None', 'Low', 'High')),
+         mod = case_when(model == 'b_fe' ~ 'Fixed Effects',
+                         model == 'b_ols' ~ 'OLS',
+                         model == 'b_pd' ~ 'Phenotype Differences'),
+         mod = factor(mod, levels=c('OLS', 'Fixed Effects', 'Phenotype Differences'))) %>%
+  filter(case == 'High') %>%
+  ggplot(aes(x=b, y=estimate, color=mod)) +
+  geom_point(alpha=0.015) +
+  geom_smooth(method=lm, formula=y~x, se=F) +
+  geom_abline(aes(slope=1, intercept=0), lty=2, color='black', alpha=0.3) +
+  facet_grid(.~mod) +
+  labs(x='Simulated Beta', y='Estimate',
+       title='Bias in Beta estimates by level of gene-environment correlation and estimator',
+       color='Estimator') +
+  theme_bw() +
+  theme(legend.position="bottom")
 
-ggsave('figs/allele_bias_500.png', width=8, height=6)
+ggsave('figs/high_allele_bias.png', width=8, height=6)
